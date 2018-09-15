b63e56cad4afd1fcfa3d31e95d3f667cf9cd435c
Mohamed El-Geish <elgeish@gmail.com>
2018-09-15 00:48:06 -0700

    Fix PS pipes

--- projects/powershell/chapter3/powershell-pipes.ps1
+++ projects/powershell/chapter3/powershell-pipes.ps1
@@ -1,21 +1,3 @@
 # Many aliases are used below for brevity
-dir | group extension | Sort-Object count | where count -gt 1 |
+dir -s * | group extension | Sort-Object count | where count -gt 1 |
 select count, name | ConvertTo-Json
-## [
-##     {
-##         "Count":  2,
-##         "Name":  ".ini"
-##     },
-##     {
-##         "Count":  3,
-##         "Name":  ".xml"
-##     },
-##     {
-##         "Count":  7,
-##         "Name":  ".log"
-##     },
-##     {
-##         "Count":  9,
-##         "Name":  ".exe"
-##     }
-## ]
