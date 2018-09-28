04c0090c8a4dfecbdcad21066f9b30fcaf453e76
Mohamed El-Geish <elgeish@gmail.com>
2018-09-28 00:48:05 -0700

    Fixes

--- projects/cpp/chapter4/operator-overloading.cpp
+++ projects/cpp/chapter4/operator-overloading.cpp
@@ -7,7 +7,9 @@ private:
   double x;
   double y;
 
-public:  
+public:
+  Point() {}
+
   // constructor accepting two arguments, called by a 
   // statement such as: Point p(1, 2);
   Point(double a_x, double a_y) : x(a_x), y(a_y) {}
