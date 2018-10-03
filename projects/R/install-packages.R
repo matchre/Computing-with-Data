7ed9d5b6f2cdcc9e96e74516142abd09061512d0
Mohamed El-Geish <elgeish@gmail.com>
2018-10-02 22:19:48 -0700

    Fixes

--- projects/R/install-packages.R
+++ projects/R/install-packages.R
@@ -1,3 +1,7 @@
+local({r <- getOption("repos")  ## assign default repo
+    r["CRAN"] <- "http://cran.r-project.org"
+    options(repos=r)
+})
 install.packages("Ecdat")
 install.packages("GGally")
 install.packages("ggplot2")
